84ba5ce67d99eb032b3f5480f4054ab6e2d4eb7d
Josh <josh@ursacreative.ca>
2023-10-02 18:02:08 -0700

    Add color

--- src/main.py
+++ src/main.py
@@ -3,13 +3,13 @@ import time
 from modules import get_current_ip, is_wifi_connected
 
 # Color pair constants
-WHITE_ON_BLACK = 1
-CYAN_ON_BLACK = 2
+RED_ON_BLACK = 1
+GREEN_ON_BLACK = 2
 
 def setup_colors():
     curses.start_color()
-    curses.init_pair(WHITE_ON_BLACK, curses.COLOR_WHITE, curses.COLOR_BLACK)
-    curses.init_pair(CYAN_ON_BLACK, curses.COLOR_CYAN, curses.COLOR_BLACK)
+    curses.init_pair(RED_ON_BLACK, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(GREEN_ON_BLACK, curses.COLOR_GREEN, curses.COLOR_BLACK)
 
 def main(stdscr):
     # Don't wait for Enter key press when calling getch()
@@ -22,13 +22,14 @@ def main(stdscr):
             stdscr.clear()
             current_time = time.strftime("%Y-%m-%d %H:%M:%S")
             current_ip = get_current_ip.get_current_ip()
-            wifi_status = "Connected to Wi-Fi" if is_wifi_connected.is_wifi_connected() else "Not connected to Wi-Fi"
+            wifi_status = is_wifi_connected.is_wifi_connected()
+            wifi_status_string = "Connected to Wi-Fi" if wifi_status else "Not connected to Wi-Fi"
 
             # Add text with custom color and attributes
-            stdscr.addstr(0, 0, f"Current Time: {current_time}", curses.color_pair(WHITE_ON_BLACK))
-            stdscr.addstr(1, 0, f"Current IP: {current_ip}", curses.color_pair(CYAN_ON_BLACK))
-            stdscr.addstr(2, 0, wifi_status, curses.color_pair(CYAN_ON_BLACK))
-            stdscr.addstr(4, 0, "------------------------------", curses.color_pair(WHITE_ON_BLACK))
+            stdscr.addstr(0, 0, f"Current Time: {current_time}")
+            stdscr.addstr(1, 0, f"Current IP: {current_ip}")
+            stdscr.addstr(2, 0, wifi_status_string, curses.color_pair(GREEN_ON_BLACK if wifi_status else RED_ON_BLACK))
+            stdscr.addstr(4, 0, "------------------------------")
             stdscr.addstr(5, 0, "")
             stdscr.refresh()
 
